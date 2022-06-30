02760fa3235d0dbc9130491654669de2423c48c8
adadesions <adadesions@gmail.com>
2022-06-30 15:06:20 +0700

    If-else

---
+++ conditions.js
@@ -0,0 +1,12 @@
+// Conditions
+// = , Assign
+let username = "Peter Parker"
+let isBioScanPass = true
+
+if (username === "adabrain") {
+    console.log("Javis: Hello Mr.", username)
+} else if (isBioScanPass) {
+    console.log("Javis: Welcome Mr.", username)
+} else {
+    console.log("Javis: Access Denied")
+}
