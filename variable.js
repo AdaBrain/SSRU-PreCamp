0a1826c808abc19a3232d970e3e30f4a64546a63
adadesions <adadesions@gmail.com>
2022-06-30 14:51:55 +0700

    Init project

---
+++ variable.js
@@ -0,0 +1,40 @@
+// Variable 
+// Only human read
+
+// Type: int (Integer)
+let x = 13
+console.log("x =", x, 'end')
+
+// Type: float (Floating point)
+let y = 3.141592
+
+// Type: String (Message, Text)
+let name = "Ada"
+let surname = "Kaminkure"
+
+// Type: Bool (true, false)
+let isSleep = false
+
+// Exercise
+// 1. Fullname (string)
+// 2. Age (int)
+// 3. Student ID (string)
+// 4. FavoriteNumber (float)
+// Display: "My name is <fullname> I'm <age> year old, "
+// "my student ID is <studentID> and I love <favNumber>."
+
+// Camel Case
+let fullname = "Arkane Kaminkure"
+let age = 18
+let studentID = "B6512345678"
+let favNumber = 3.141592
+
+console.log("My name is", fullname, "I'm", age, "years old")
+console.log("my student ID is", studentID, "and I love", favNumber)
+
+// let keyword --> Local scope
+// var keyword --> Global scope
+// const keyword --> Constant are immutable
+
+const book = "Harry Potter"
+
