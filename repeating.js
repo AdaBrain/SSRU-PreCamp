552698a7456c7df00f59189b064e240a8b7ca29e
adadesions <adadesions@gmail.com>
2022-06-30 16:11:53 +0700

    While loop

--- repeating.js
+++ repeating.js
@@ -13,3 +13,11 @@ for (let i=1; i <= limit; i++) {
     // sum = sum + i
 }
 console.log("Sum from 1-100 =", sum)
+
+// While Loop
+let input = 32
+while (input % 7 < 6) {
+    console.log("r =", input % 7, "input =", input)
+    input--
+}
+console.log("END!")
