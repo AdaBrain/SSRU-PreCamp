7a1d13f15508ae6552fa279bfb306a75e241d8d6
adadesions <adadesions@gmail.com>
2022-06-30 15:31:16 +0700

    Complete problem1

--- problem1.js
+++ problem1.js
@@ -4,11 +4,17 @@
 // 2. print "Buzz" when input divisible by 5
 // 3. print "FizzBuzz" when input divisible by 3 and 5
 
-let input = 15
+let input = 6
 // && --> AND Operator
 // % --> Modulo (mod) operator, divide for remainder
 console.log("input =", input)
 
 if (input % 3 === 0 && input % 5 === 0) {
     console.log("FizzBuzz")
+} else if (input % 3 === 0) {
+    console.log("Fizz")
+} else if (input % 5 === 0) {
+    console.log("Buzz")
+} else {
+    console.log("Input can't divides by 3 and 5: NoFizz NoBuzz")
 }
