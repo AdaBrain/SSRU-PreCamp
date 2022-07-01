b363023eee7659f5baaa68270d063c707f11122e
adadesions <adadesions@gmail.com>
2022-07-01 13:45:31 +0700

    Change card component to generative form

--- my-first-web/src/components/Card.js
+++ my-first-web/src/components/Card.js
@@ -1,16 +1,15 @@
 import "../assets/ada.css";
-import pxArt from "../assets/pxArt.jpeg"
 
 // Card Components
-const Card = () => {
+const Card = (props) => {
+  const { title, content, img } = props
+
   return (
     <div className="card-container">
-      <img className="pixel-img" src={pxArt} alt="" />
-      <h1>This is my Card!</h1>
+      <img className="pixel-img" src={img} alt="" />
+      <h1>{title}</h1>
       <p>
-        Runs the app in the development mode. Open http://localhost:3000 to view
-        it in your browser. The page will reload when you make changes. You may
-        also see any lint errors in the console.
+        {content}
       </p>
     </div>
   );
