9da16332edfbd7567374c81ce6e618bdc335d055
adadesions <adadesions@gmail.com>
2022-07-01 11:18:22 +0700

    Init project web

---
+++ my-first-web/src/App.js
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>AdaBrain is HERE</h1>
+      <p>
+        A Leader in the 2021 Gartner Magic Quadrant for Meeting Solutions
+        Gartner, the world's leading information technology research and
+        advisory firm, has named Zoom a Leader in the 2021 Magic Quadrant for
+        Meeting Solutions. Read the report to learn why we're a Leader!
+      </p>
+
+      <a href="https://google.com">Google.com</a>
+    </div>
+  );
+}
+
+export default App;
