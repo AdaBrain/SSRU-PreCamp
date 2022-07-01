434280c25734b34f9f5df4b7304efc166cb9a3d6
adadesions <adadesions@gmail.com>
2022-07-01 14:59:28 +0700

    Deploy to firebase

--- my-first-web/src/App.js
+++ my-first-web/src/App.js
@@ -1,19 +1,11 @@
 import pxArt from "./assets/pxArt.jpeg";
 import "./assets/ada.css";
 import Card from "./components/Card";
-import cardData from "./assets/data/cardsData.json"
+import cardData from "./assets/data/cardsData.json";
 
 function App() {
   return (
     <div className="App">
-      <h1>AdaBrain is HERE</h1>
-      <p>
-        A Leader in the 2021 Gartner Magic Quadrant for Meeting Solutions
-        Gartner, the world's leading information technology research and
-        advisory firm, has named Zoom a Leader in the 2021 Magic Quadrant for
-        Meeting Solutions. Read the report to learn why we're a Leader!
-      </p>      
-
       {/* <img id="ada-img" className="pixel-img" src={pxArt} alt="Pixel Art" />
       <img className="pixel-img" src={pxArt} alt="Pixel Art" /> */}
 
@@ -26,7 +18,6 @@ function App() {
           img={data.img}
         />
       ))}
-
     </div>
   );
 }
