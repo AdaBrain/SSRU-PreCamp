2563766b98b36ba236267cd1a4bf75d0f17b5578
adadesions <adadesions@gmail.com>
2022-07-01 15:21:54 +0700

    Complete CardHZ component

--- my-first-web/src/App.js
+++ my-first-web/src/App.js
@@ -2,6 +2,7 @@ import pxArt from "./assets/pxArt.jpeg";
 import "./assets/ada.css";
 import Card from "./components/Card";
 import cardData from "./assets/data/cardsData.json";
+import CardHZ from "./components/CardHZ";
 
 function App() {
   return (
@@ -9,6 +10,17 @@ function App() {
       {/* <img id="ada-img" className="pixel-img" src={pxArt} alt="Pixel Art" />
       <img className="pixel-img" src={pxArt} alt="Pixel Art" /> */}
 
+      <CardHZ
+        img="https://i.pinimg.com/originals/6f/e6/83/6fe683d1b0c6d53cef3f4377e3a3f520.jpg"
+        title="My Personality"
+        content="magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea"
+      />    
+
+      <CardHZ
+        img="https://i.pinimg.com/originals/6f/e6/83/6fe683d1b0c6d53cef3f4377e3a3f520.jpg"
+        title="My Personality"
+        content="magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea"
+      />    
       {/* Render Cards Components */}
       {cardData.map((data, idx) => (
         <Card
--- my-first-web/src/components/CardHZ.js
+++ my-first-web/src/components/CardHZ.js
@@ -1,13 +1,15 @@
 const CardHZ = (props) => {
-    const {img, title, content} = props
-    
-    return (
-        <div className="card-hz-container">
-            <img src={img} alt="Not loading" />
-            <h2>{title}</h2>
-            <p>{content}</p>
-        </div>
-    )
-}
+  const { img, title, content } = props;
 
-export default CardHZ
+  return (
+    <div className="card-hz-container">
+      <img src={img} alt="Not loading" />
+      <div className="card-hz-content">
+        <h2>{title}</h2>
+        <p>{content}</p>
+      </div>
+    </div>
+  );
+};
+
+export default CardHZ;
