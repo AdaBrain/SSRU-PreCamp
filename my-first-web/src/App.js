c05a9d287a9a1d03916a0baa20f027b65f2c16d2
adadesions <adadesions@gmail.com>
2022-07-01 15:40:18 +0700

    Adjust content

--- my-first-web/src/App.js
+++ my-first-web/src/App.js
@@ -13,22 +13,25 @@ function App() {
         img="https://i.pinimg.com/originals/6f/e6/83/6fe683d1b0c6d53cef3f4377e3a3f520.jpg"
         title="My Personality"
         content="magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea"
-      />    
+      />
 
       <CardHZ
         img="https://i.pinimg.com/originals/6f/e6/83/6fe683d1b0c6d53cef3f4377e3a3f520.jpg"
         title="My Personality"
         content="magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea"
-      />    
-      {/* Render Cards Components */}
-      {cardData.map((data, idx) => (
-        <Card
-          key={idx}
-          title={data.title}
-          content={data.content}
-          img={data.img}
-        />
-      ))}
+      />
+
+      <div className="card-vertical-zone">
+        {/* Render Cards Components */}
+        {cardData.map((data, idx) => (
+          <Card
+            key={idx}
+            title={data.title}
+            content={data.content}
+            img={data.img}
+          />
+        ))}
+      </div>
     </div>
   );
 }
